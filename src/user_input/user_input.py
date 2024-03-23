9ce2dd327c7c65fbbeb6ab49c34cfb196fd0833a
salochinYom <nrmoy2@gmail.com>
2024-03-22 22:00:20 -0400

    fixed spelling

--- src/user_input/user_input.py
+++ src/user_input/user_input.py
@@ -7,17 +7,17 @@ class user_input:
         #would theoretically come from a list of all recipies
         self.ingredients = ingredients_list
 
-    def get_alergies(self):
+    def get_allergies(self):
         return self.allergies
 
     def get_preferences(self):
         return self.preferences
 
     def get_user_inputs(self):
-        self.get_user_alergies()
+        self.get_user_allergies()
         self.get_user_preferences
 
-    #todo: add functionality to ensure that preferences cannot be alergies
+    #todo: add functionality to ensure that preferences cannot be allergies
     def get_user_preferences(self):
         #reset preferences list
         self.preferences = []
@@ -26,12 +26,12 @@ class user_input:
         #handle first case
         text = input("do you have any preferences? if none enter 'no'")
 
-    def get_user_alergies(self):
-        # reset the alergies
+    def get_user_allergies(self):
+        # reset the allergies
         self.allergies = []
         repeat = True
-        # handle the first case for alergies
-        text = input("what is one of your food algeries? if none enter 'no': ")
+        # handle the first case for allergies
+        text = input("what is one of your food allergies? if none enter 'no': ")
         #check if the text is no
         if text.lower() == "no":
             repeat = False
@@ -40,21 +40,21 @@ class user_input:
             inlist = False
             for item in self.ingredients:
                 if item == text.lower():
-                    #add the thing to the alergy list
+                    #add the thing to the allergy list
                     self.allergies.append(text.lower())
                     inlist = True
                     #stop the for loop
                     break
             if not inlist:
-                print(text.lower() + " is not a ingredient in any of our recipies. we are not it appending to list")
+                print(text.lower() + " is not a ingredient in any of our recipes. we are not it appending to list")
 
-        # prompt the user to add aditional
+        # prompt the user to add additional
         while repeat:
-            #display the current alergy list
+            #display the current allergy list
             print("your allergies are " + str(self.allergies))
 
-            #prompt user for aditional allergies
-            text = input("what is another one of your food algeries? if none addition enter 'no': ")
+            #prompt user for additional allergies
+            text = input("what is another one of your food allergies? if none addition enter 'no': ")
             # check if the text is no
             if text.lower() == "no":
                 repeat = False
@@ -63,13 +63,13 @@ class user_input:
                 inlist = False
                 for item in self.ingredients:
                     if item == text.lower():
-                        # add the thing to the alergy list
+                        # add the thing to the allergy list
                         self.allergies.append(text.lower())
                         inlist = True
                         # stop the for loop
                         break
                 if not inlist:
-                    print(text.lower() + " is not a ingredient in any of our recipies. we are not it appending to list")
+                    print(text.lower() + " is not a ingredient in any of our recipes. we are not it appending to list")
 
 def main():
     #for testing user interface.
