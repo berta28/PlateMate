d5078e11243ab727f7117d8d90e2aa04c571dd39
berta28 <126499776+berta28@users.noreply.github.com>
2024-04-29 20:35:34 -0500

    working

--- src/rnn_meal_generator.py
+++ src/rnn_meal_generator.py
@@ -10,10 +10,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-
+from torchvision import transforms
+from models.meal_plan import MealPlan, Recipe, GroceryList, Ingredient
 import retrieve_training_data
 from models.dataset import Dataset
+from user_input.user_input import user_input
 
 class RNNMealGenerator:
     def __init__(self):
@@ -34,7 +35,7 @@ class RNNMealGenerator:
         num_epochs = 100
 
         # Initialize RNN model
-        model = nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=output_size ,batch_first=True)
+        model = nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=1 ,batch_first=True)
         # Define loss function and optimizer
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -73,9 +74,9 @@ class RNNMealGenerator:
 
         #retrieve the user and mealPlan at index 0
         tde = retrieve_training_data.training_data_extractor()
-        dataset = Dataset()
-        dataset.create_recipes_from_csv(file_location="data/recipes/full.json")
-        ingredient_list = dataset.get_ingredient_names()
+        self.dataset = Dataset()
+        self.dataset.create_recipes_from_csv(file_location="data/recipes/full.json")
+        ingredient_list = self.dataset.get_ingredient_names()
         
         # Define variables
         features = []
@@ -115,12 +116,37 @@ class RNNMealGenerator:
         model.eval()
         with torch.no_grad():
             test_input = torch.tensor(test_feature,dtype=torch.float32)
-            predictions = model(test_input)
+            predictions, hidden = model(test_input)
             print(predictions)
         
         self.model = model
 
+    def predict(self, user):
+        test_input = self.convert_input_to_tensor(user)
+        with torch.no_grad():
+            predictions, hidden = self.model(test_input)
+            #convert predictions to meal plan
+            #print(predictions)
+            print(predictions)
+            #count the number of recipes in the dataset
+            number_of_recipes = len(self.dataset.recipes)
+            
+            #multiple by probability of each recipe
+            recipe_list = []
+            for prob in predictions:
+                for pro in prob:
+                    recipe_id = int(abs(pro)*number_of_recipes)
+                    recipe = self.dataset.recipes[recipe_id]
+
+                    recipe_list.append(recipe)
+                
+        for recipe in recipe_list:
+            print(recipe.title)
+        return MealPlan(recipes=recipe_list, grocery_list=GroceryList(ingredients=Ingredient("None")), estimated_cost=1)
 
 if __name__ == "__main__":
     rnn_meal_generator = RNNMealGenerator()
     rnn_meal_generator.create_model()
+    user = user_input(rnn_meal_generator.dataset.get_ingredient_names())
+    input = rnn_meal_generator.convert_input_to_tensor(user)
+    p = rnn_meal_generator.predict(user)
